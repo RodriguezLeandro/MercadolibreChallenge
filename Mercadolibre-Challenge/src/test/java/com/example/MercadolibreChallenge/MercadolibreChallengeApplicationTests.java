fe12a1128e6832d4c8b330063ce5d19b0ad63c03
RodriguezLeandro <leandro21890000@gmail.com>
2022-03-10 20:09:30 -0300

    Initial challenge autogenerated spring boot restful web service template

---
+++ Mercadolibre-Challenge/src/test/java/com/example/MercadolibreChallenge/MercadolibreChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MercadolibreChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolibreChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
